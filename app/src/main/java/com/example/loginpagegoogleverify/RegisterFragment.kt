a548458c4e0ad7826507c2d3bc4a11767865f8a9
FaribaKhandani <your_email_address@example.com>
2024-03-18 11:35:09 +0100

    fragment added

--- app/src/main/java/com/example/loginpagegoogleverify/RegisterFragment.kt
+++ app/src/main/java/com/example/loginpagegoogleverify/RegisterFragment.kt
@@ -1,59 +1,194 @@
 package com.example.loginpagegoogleverify
 
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.loginpagegoogleverify.databinding.FragmentRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var registerBinding: FragmentRegisterBinding
+    lateinit var viewModelLogin: ViewModelLogin
+
+    var state = false
+    var matchFound = false
+
+    val emailVerify = MutableLiveData<Boolean>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+
+        val dao = LoginDatabase.getInstance(requireContext()).daoLogin
+        val repository = RepositoryLogin(dao)
+        val factory = FactoryLogin(repository)
+
+
+
+
+        registerBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
+
+
+
+
+        viewModelLogin = ViewModelProvider(this, factory).get(ViewModelLogin::class.java)
+        registerBinding.mvvmregister = viewModelLogin
+
+
+
+
+
+        registerBinding.bottonregister.setOnClickListener {
+
+            var userss = viewModelLogin.userlist
+            matchFound = false
+            Log.e("users8",userss.size.toString())
+            for(i in userss.indices){
+
+
+                if(userss[i].username.toString() == registerBinding.frgmntregEtUsername.text.toString()){
+                    Log.e("users9", userss[i].username)
+                    matchFound = true
+                    break
                 }
             }
+
+
+
+            emailVerify.value = !matchFound
+
+            Log.e("users5",emailVerify.value.toString())
+
+            if (viewModelLogin.registerButton() == true && emailVerify.value ==true) {
+
+
+
+                val sharedPreferences = requireActivity().getSharedPreferences(
+                    "MyPreferences",
+                    Context.MODE_PRIVATE
+                )
+                val editor = sharedPreferences.edit()
+                var preferenceValue = registerBinding.frgmntregEtUsername.text.toString()
+
+                editor.putString("emailAddress", preferenceValue.toString());
+                editor.apply();
+                val intent = Intent(activity, BlankFragment::class.java)
+                startActivity(intent)
+
+
+
+
+
+
+            }
+
+            else
+            {
+                Toast.makeText(requireContext(), "Email Address or Password is invalid", Toast.LENGTH_LONG).show()
+
+
+
+
+            }
+
+
+
+        }
+
+
+
+
+        viewModelLogin.emailResult.observe(requireActivity(), Observer {
+
+            if (it == true) {
+
+                registerBinding.frgmntregTvUsercheck.visibility = View.VISIBLE
+                registerBinding.frgmntregTvUsercheck.hint = "Valid email address"
+                registerBinding.frgmntregTvUsercheck.setHintTextColor(Color.GREEN)
+
+
+            } else {
+                registerBinding.frgmntregTvUsercheck.visibility = View.VISIBLE
+                registerBinding.frgmntregTvUsercheck.hint = "Invalid email address"
+                registerBinding.frgmntregTvUsercheck.setHintTextColor(Color.RED)
+            }
+        })
+
+
+        viewModelLogin.passwordResult.observe(requireActivity(), Observer {
+            if (it == true) {
+                registerBinding.frgmntregTvPasscheck.visibility = View.VISIBLE
+                registerBinding.frgmntregTvPasscheck.hint = "Valid Password"
+                registerBinding.frgmntregTvPasscheck.setHintTextColor(Color.GREEN)
+
+
+            } else {
+                registerBinding.frgmntregTvPasscheck.visibility = View.VISIBLE
+                registerBinding.frgmntregTvPasscheck.hint =
+                    "Minimum 8 characters,Uppercase\n Lowercase,numeric and special character"
+                registerBinding.frgmntregTvPasscheck.setHintTextColor(Color.RED)
+            }
+        })
+
+
+        darkmodeCheck()
+        return registerBinding.root
     }
+
+
+
+
+
+
+    fun darkmodeCheck(){
+        val isDarkMode = isDarkModeEnabled(requireContext())
+        if (isDarkMode) {
+
+            registerBinding.frgmntregTvFirstname.setTextColor(android.graphics.Color.WHITE)
+            registerBinding.frgmntregTvLastname.setTextColor(android.graphics.Color.WHITE)
+            registerBinding.frgmntregTvEmailadd.setTextColor(android.graphics.Color.WHITE)
+            registerBinding.frgmntregTvPassword.setTextColor(android.graphics.Color.WHITE)
+            registerBinding.frgmntregTvSignup.setTextColor(android.graphics.Color.WHITE)
+            registerBinding.frgmntregEtUsername.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregEtPassword.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregEtLastname.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregEtFirstname.setTextColor(android.graphics.Color.BLACK)
+
+        } else {
+
+            registerBinding.frgmntregTvFirstname.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregTvLastname.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregTvEmailadd.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregTvPassword.setTextColor(android.graphics.Color.BLACK)
+            registerBinding.frgmntregTvSignup.setTextColor(android.graphics.Color.BLACK)
+        }
+    }
+
+    fun isDarkModeEnabled(context: Context): Boolean {
+        val currentNightMode = context.resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK
+
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
+    }
+
+
+
 }
